89427ac18208f89e9701c8ce4d59e8ac3bf3b416
prashant-mudgal <prashant.mudgal@jet2.com>
2020-04-16 18:34:26 +0530

    test comment

--- app/src/main/java/com/retrofitexample/MainActivity.java
+++ app/src/main/java/com/retrofitexample/MainActivity.java
@@ -14,8 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements NetworkResponseListener<List<Hero>> {
-//test
-//test1
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
